3ee80231795cda4f065849a286fe98e5333e5d69
collinskoech11 <collinskoechck34@gmail.com>
2021-11-14 10:53:10 +0300

    update product fields in items

--- amazontutorial/spiders/amazon_spider.py
+++ amazontutorial/spiders/amazon_spider.py
@@ -11,7 +11,14 @@ class AmazonSpiderSpider(scrapy.Spider):
         items = AmazontutorialItem()
 
 
-        product_name = response.css('.p13n-sc-truncated').extract()
-        product_author = response.css('.a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').get()
-        product_price = response.css('..p13n-sc-price , .zg-item-immersion:nth-child(14) .a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').get()
-        product_imagelink = response.css('img , #zg-other-container+ .zg-item-immersion .p13n-sc-price , .zg-item-immersion:nth-child(14) .a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').get()
+        product_name = response.css('.p13n-sc-truncated::text').extract()
+        product_author = response.css('.a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').css('::text').extract()
+        product_price = response.css('..p13n-sc-price , .zg-item-immersion:nth-child(14) .a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').css('::text').extract()
+        product_imagelink = response.css('img , #zg-other-container+ .zg-item-immersion .p13n-sc-price , .zg-item-immersion:nth-child(14) .a-link-child , .zg-item-immersion:nth-child(2) .p13n-sc-truncated').css('::alt(src)').extract()
+
+        items['product_name'] = product_name
+        items['product_author'] = product_author
+        items['product_price'] = product_price
+        items['product_imagelink'] = product_imagelink
+
+        yield items
